20a597b692663a5e8d91a9d41e269eae59e4a8d9
mawc2019 <mawc@mit.edu>
2023-03-12 20:49:24 -0400

    change __init__.py

--- ruler/__init__.py
+++ ruler/__init__.py
@@ -1,3 +1,1 @@
-from .ruler import solid_minimum_length, void_minimum_length, both_minimum_length, dual_minimum_length, heaviside_open, heaviside_close
-
-# Some code is copied or adapted from Meep at https://github.com/smartalecH/meep/blob/jax_rebase/python/adjoint/filters.py 
+from .ruler import solid_minimum_length, void_minimum_length, both_minimum_length, dual_minimum_length, binary_open, binary_close, binary_erode, binary_dilate
